6007af13bfa0a07074876f8dff2506457a7f7c50
SanxgoStark <17030203@itcelaya.edu.mx>
2021-11-12 17:46:13 -0600

    gui agendar citas paciente con adicion de bootstrap

--- views/citas/test.php
+++ views/citas/test.php
@@ -1,79 +1,64 @@
 
 <!DOCTYPE html>
 <html>
-<head>
+<head>   
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
   <title>Agendar cita</title>
 
   <style>
-ul {
-  list-style-type: none;
-  height:60px;
-  margin: 0;
-  padding: 0;
-  overflow: hidden;
-  background-color: white;
-  border:black 1px solid
-}
-
-li {
-    margin-top: 22px;
-    margin-left: 30px;
-  float: left;
-
-}
-
-li a {
-  display: block;
-  padding: 8px;
-}
-</style>
-
+  </style>
 </head>
 <body>
 
-<div style="">
-<ul>
-  <li style=""><a style="color:black" href="#home">Registrar cita</a></li>
-  <li><a style="color:black" href="#news">Registrar paciente</a></li>
-  <li><a style="color:black" href="#contact">Preescribir receta</a></li>
-</ul>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Registrar paciente <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Registrar cita</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Prescribir receta</a>
+      </li>
+    </ul>
+  </div>
+</nav>
 
-</div>
-
-<div style="background:;height: auto;">
+<div style="background:;height: auto;" class="">
 <h1 style="margin-left:20px;">Agendar cita</h3>
 
 <div>
 
 <form style="background:" action="pacientes.php?action=agendarcita" method="POST" enctype="multipart/form-data">
 
-<div style="margin-left:50px;margin-top:40px" class="form-floating mb-3">
-<label for="floatingInput">Nombre completo del paciente</label>
-    <input style="margin-left:80px;width:800px"type="text" name="paciente[nombre]" value="" class="form-control"  placeholder="Ingresar" style=" width: 526px">
-    
+<div style="margin-left:50px;margin-top:60px" class="row">
+<label>Nombre completo del paciente</label>
+    <input style="margin-left:150px;width:750px"type="text" name="paciente[nombre]" value="" class="form-control"  placeholder="Ingresar" style="">  
   </div>
 
-  <div style="margin-left:50px;margin-top:70px" class="form-floating mb-3">
+  <div style="margin-left:50px;margin-top:60px" class="row">
   <label for="floatingInput">Descripcion de la cita</label>
-    <input style="margin-left:260px;width:800px" type="text" name="paciente[agendarcita]" value="" class="form-control" placeholder="Ingresar" style=" width: 526px">
+    <input style="margin-left:310px;width:750px" type="text" name="paciente[agendarcita]" value="" class="form-control" placeholder="Ingresar" style=" width:">
     
   </div>
 
-  <div style="margin-left:50px;margin-top:70px" class="form-floating mb-3">
+  <div style="margin-left:50px;margin-top:60px" class="row">
   <label for="floatingInput">Fecha de la cita</label>
-    <input style="margin-left:298px;width:" type="date" name="paciente[amaterno]" value="" class="form-control"  placeholder="Fecha Cita" style=" width: 526px">
+    <input style="margin-left:350px;width:250px" type="date" name="paciente[amaterno]" value="" class="form-control"  placeholder="Fecha Cita" style="">
    
   </div>
 
-  <div style="margin-left:50px;margin-top:70px" class="form-floating mb-3">
+  <div style="margin-left:50px;margin-top:60px" class="row">
   <label for="floatingInput">Hora de la cita</label>
-    <input style="margin-left:305px;width:800px" type="text" name="paciente[nacimiento]" value="" class="form-control" placeholder="Ingresar" style=" width: 526px">
+    <input style="margin-left:355px;width:755px" type="text" name="paciente[nacimiento]" value="" class="form-control" placeholder="Ingresar" style="">
     
   </div>
 
   
-  <div style="margin-left:1130px;margin-top:90px" class="form-group">
-    <input style="color: white;background:black;width:130px;height:40px" type="submit" name="enviar" value="Guardar" class="btn btn-success" />
+  <div style="margin-left:1130px;margin-top:76px" class="form-group">
+    <input style="color: white;background:black;width:130px;height:40px;border-color:black" type="submit" name="enviar" value="Guardar" class="btn btn-success" />
   </div>
 
 </form>
