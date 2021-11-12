ea26b1102dde6d4d269aba29d0dbd3b027fde9f7
edgar <edgar59156@gmail.com>
2021-11-12 16:26:59 -0600

    Citas

--- views/recetas/form.php
+++ views/recetas/form.php
@@ -1,3 +1,4 @@
+<!--Comentario-->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
 <title>Consultorio Odontológico</title>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
